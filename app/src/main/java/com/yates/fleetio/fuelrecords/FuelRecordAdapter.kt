474e7d851d0ac63b71e472be54f703666fbebfb6
brandonyates <brandonyates66@gmail.com>
2021-11-01 09:36:15 -0500

    final cleanup

--- app/src/main/java/com/yates/fleetio/fuelrecords/FuelRecordAdapter.kt
+++ app/src/main/java/com/yates/fleetio/fuelrecords/FuelRecordAdapter.kt
@@ -5,8 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.yates.fleetio.R
 import com.yates.fleetio.api.FleetioDateFormat
 import com.yates.fleetio.databinding.FuelRecordRowItemBinding
+import java.text.SimpleDateFormat
 
 class FuelRecordAdapter : ListAdapter<FuelRecord, FuelRecordAdapter.FuelRecordViewHolder>(FuelRecordDiffCallback) {
 
@@ -20,17 +22,23 @@ class FuelRecordAdapter : ListAdapter<FuelRecord, FuelRecordAdapter.FuelRecordVi
     override fun onBindViewHolder(holder: FuelRecordAdapter.FuelRecordViewHolder, position: Int) {
         currentList[position].also { record ->
             with(holder) {
-                binding.tvDate.text = FleetioDateFormat.format(record.date)
+                binding.tvDate.text = SimpleDateFormat("yyyy-MM-dd HH:mm").format(record.date)
                 binding.tvVehicleName.text = record.vehicle_name
-                binding.tvCost.text = record.cost_per_hr.toString()
-                binding.tvCpm.text = record.cost_per_mi.toString()
-                binding.tvFuelType.text = record.fuel_type_name
+                binding.tvCost.text = record.cost_per_hr.getText(binding)
+                binding.tvCpm.text = record.cost_per_mi.getText(binding)
+                binding.tvFuelType.text = record.fuel_type_name.getText(binding)
                 binding.tvGallons.text = record.us_gallons.toString()
-                binding.tvPpg.text = record.price_per_volume_unit.toString()
+                binding.tvPpg.text = record.price_per_volume_unit.getText(binding)
                 binding.tvRefNum.text = record.id.toString()
             }
         }
     }
+
+    private fun Float?.getText(binding: FuelRecordRowItemBinding) =
+        this?.toString() ?: binding.root.resources.getText(R.string.notAvailable)
+
+    private fun String?.getText(binding: FuelRecordRowItemBinding) =
+        this?.toString() ?: binding.root.resources.getText(R.string.notAvailable)
 
     override fun getItemCount(): Int = currentList.size
 
